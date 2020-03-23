d3a55e4177ff781b41cea86ac3e7574a1fb996a6
khalifa005 <mahmudkhalifa12@outlook.com>
2020-03-23 14:18:24 +0200

    initial stage

---
+++ MIgrations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DailyLog.Migrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
